459eaf980acf7f8ed8e05f28d874eeda79bbc7b5
‎Chanathip Ruangphueng <chanathip.p2545@gmail.com>
2026-09-22 11:33:28 +0700

    Update report.js

--- report.js
+++ report.js
@@ -254,7 +254,15 @@ async function handleReportSubmit(event) {
         ticket: ticket,
 
         dateTime:
-            new Date().toISOString(),
+            const now = new Date();
+
+const dateTime =
+    now.getFullYear() + "-" +
+    String(now.getMonth() + 1).padStart(2, "0") + "-" +
+    String(now.getDate()).padStart(2, "0") + " " +
+    String(now.getHours()).padStart(2, "0") + ":" +
+    String(now.getMinutes()).padStart(2, "0") + ":" +
+    String(now.getSeconds()).padStart(2, "0");
 
         user:
             currentUser.username ||
